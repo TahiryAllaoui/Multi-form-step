dde59cfb9742f1d0c7508c1e62b908ccbc79c7d4
TahiryAllaoui <tallaoui31@gmail.com>
2023-08-13 20:09:43 +0300

    Security on Personal Info going well

--- src/components/pages/PersonalInfo/PersonalInfo.tsx
+++ src/components/pages/PersonalInfo/PersonalInfo.tsx
@@ -1,25 +1,98 @@
 import { Link } from 'react-router-dom';
 import './style.scss'
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Identity } from '../../utils/Info';
+
+interface etat {
+    value: string;
+    error: boolean;
+}
 
 
 const PersonalInfo = () => {
     const specialChar: RegExp = /[\`1234567890=\/\*\+\{\}\~\!\@\#\$\%\|\^\&\*\(\)\=\[\]\:\'\"\;\,\?\>\<]/;
+    const specialCharForNumber: RegExp = /[\`qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM=\/\*\+\{\}\~\!\@\#\$\%\|\^\&\*\(\)\=\[\]\:\'\"\;\,\?\>\<]/;
+    const specialCharForMail: RegExp = /[\`=\/\*\+\{\}\~\!\\\#\$\%\|\^\&\*\(\)\=\[\]\:\'\"\;\,\?\>\<]/;
 
     const context = useContext(Identity);
 
+    //Handling Input Change
     const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
-        context?.setName(e.currentTarget.value);
+        context!.setName(e.currentTarget.value);
     };
 
     const handleMail = (e: React.ChangeEvent<HTMLInputElement>) => {
-        context?.setMail(e.currentTarget.value);
+        context!.setMail(e.currentTarget.value);
     };
 
     const handleNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
-        context?.setNumber(e.currentTarget.value);
+        context!.setNumber(e.currentTarget.value);
     };
+
+    //handling Input error
+    // const [nameError, setNameError] = useState<etat>({
+    //     value: context!.name,
+    //     error: true
+    // })
+    // const [mailError, setMailError] = useState<etat>({
+    //     value: context!.mail,
+    //     error: true
+    // })
+    // const [numberError, setNUmberError] = useState<etat>({
+    //     value: context!.number,
+    //     error: true
+    // })
+    const [buttonIsDisabled, setButtonIsDisabled] = useState(true);
+
+    useEffect(() => {
+        let buttonState: boolean = false;
+
+        //Contrlling void input
+        if (context!.name == '') {
+            buttonState = true;
+        }
+
+        if (context!.number == '') {
+            buttonState = true;
+        }
+
+        if (context!.name == '') {
+            buttonState = true;
+        }
+
+        //Controlling non-void input
+        if (specialChar.test(context!.name)) {
+            let myInput = document.querySelector('.input-1');
+            myInput!.classList.add('vibrate');
+            buttonState = true;
+        }
+        else {
+            let myInput = document.querySelector('.input-1');
+            myInput!.classList.remove('vibrate');
+        }
+
+        if (specialCharForMail.test(context!.mail)) {
+            let myInput = document.querySelector('.input-2');
+            myInput!.classList.add('vibrate');
+            buttonState = true;
+        }
+        else {
+            let myInput = document.querySelector('.input-2');
+            myInput!.classList.remove('vibrate');
+        }
+
+        if (specialCharForNumber.test(context!.number)) {
+            let myInput = document.querySelector('.input-3');
+            myInput!.classList.add('vibrate');
+            buttonState = true;
+        }
+        else {
+            let myInput = document.querySelector('.input-3');
+            myInput!.classList.remove('vibrate');
+        }
+        setButtonIsDisabled(buttonState);
+    }, [context!.name, context!.mail, context!.number])
+
 
 
 
@@ -34,15 +107,15 @@ const PersonalInfo = () => {
                 <div className="form-elements">
                     <div className="form-item">
                         <label>Name</label>
-                        <input type="text" required placeholder='e.g. Stephen King' onChange={handleName} value={context?.name} />
+                        <input type="text" required placeholder='e.g. Stephen King' onChange={handleName} value={context?.name} className='input-1' />
                     </div>
                     <div className="form-item">
                         <label>Email Address</label>
-                        <input type="email" required placeholder='e.g. stephenking@gmail.com' onChange={handleMail} value={context?.mail} />
+                        <input type="email" required placeholder='e.g. stephenking@gmail.com' onChange={handleMail} value={context?.mail} className='input-2' />
                     </div>
                     <div className="form-item">
                         <label>Phone Number</label>
-                        <input type="text" required placeholder='e.g. +1 234 567 853' onChange={handleNumber} value={context?.number} />
+                        <input type="text" required placeholder='e.g. +1 234 567 853' onChange={handleNumber} value={context?.number} className='input-3' />
                     </div>
                 </div>
             </div>
@@ -51,7 +124,7 @@ const PersonalInfo = () => {
 
                 </Link>
                 <Link to='/select-plan' className='next-Link'>
-                    <button>Next step</button>
+                    <button disabled={buttonIsDisabled}>Next step</button>
                 </Link>
             </div>
         </form>
